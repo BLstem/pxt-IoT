8b9b1e315870d09eeb277c87a72fb8588eb48cdb
DoraLC <34760377+DoraLC@users.noreply.github.com>
2018-06-14 13:37:14 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -11,7 +11,6 @@ namespace ESP8266 {
     //% rx.fieldEditor="gridpicker" rx.fieldOptions.columns=3
     //% rx.fieldOptions.tooltips="false"
     //% weight=82
-    //% blockGap=7  
     export function initializeWifi(tx: SerialPin, rx:SerialPin): void {
         serial.redirect(tx,rx,BaudRate.BaudRate115200);
         serial.onDataReceived(serial.delimiters(Delimiters.NewLine), () => {});
@@ -24,7 +23,6 @@ namespace ESP8266 {
     //% blockId=esp8266_set_wifi
     //% block="Set WiFi to ssid %ssid| pwd %pwd"   
     //% weight=81  
-    //% blockGap=7  
     export function setWifi(ssid: string, pwd: string): void {
         serial.writeString("AT+RST\r\n");
         serial.writeString("AT+CWMODE=1\r\n");
@@ -38,7 +36,6 @@ namespace ESP8266 {
     //% blockId=esp8266_set_thingspeak
     //% block="Send ThingSpeak key %key| field1 %field1"
     //% weight=78
-    //% blockGap=7  
     export function sendThingspeak(key: string, field1: number): void {
         let message = "GET /update?api_key=" + key + "&field1=" + field1 + "\r\n\r\n";
         serial.writeString("AT+CIPMUX=0\r\n");
@@ -55,7 +52,6 @@ namespace ESP8266 {
     //% blockId=esp8266_set_thingspeak_list
     //% block="Send ThingSpeak key %key| array %fields"
     //% weight=77
-    //% blockGap=7  
     export function sendThingspeakwithArray(key: string, fields: number[]): void {
         let message = "GET /update?api_key=" + key + "&";
         for(let i=0;i<fields.length;i++){
@@ -80,7 +76,6 @@ namespace ESP8266 {
     //% blockId=esp8266_set_ifttt
     //% block="Send IFTTT key %key|event_name %eventname|value %value"
     //% weight=80   
-    //% blockGap=7  
     export function sendIFTTT(key: string, eventname: string, value: number): void {
         let message = "GET /trigger/" + eventname + "/with/key/" + key + "?value1=" + value + " HTTP/1.1\r\nHost: maker.ifttt.com\r\nConnection: close\r\n\r\n";
         serial.writeString("AT+CIPMUX=0\r\n");
@@ -97,7 +92,6 @@ namespace ESP8266 {
     //% blockId=esp8266_set_ifttt_list
     //% block="Send IFTTT key %key|event_name %eventname|array %values"
     //% weight=79
-    //% blockGap=7  
     export function sendIFTTTwithArray(key: string, eventname: string, values: number[]): void {
         let message = "GET /trigger/" + eventname + "/with/key/" + key + "?";
         for(let i=0;i<values.length;i++){
